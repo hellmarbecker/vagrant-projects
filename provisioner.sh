dda91271d84944efbd13829c66a56229c5162172
rq89xc <hellmar.becker@ing.nl>
2015-01-03 15:43:31 +0100

    this one gets stuck waiting for Ambari server

--- provisioner.sh
+++ provisioner.sh
@@ -16,17 +16,13 @@ then
 fi
 touch $tagfile
 
+# Distribute root's public ssh key to all nodes.
 mkdir -p /root/.ssh
 echo 'Copying public root SSH Key to VM for provisioning...'
 echo "$1" > /root/.ssh/id_rsa.pub
 chmod 600 /root/.ssh/id_rsa.pub
 cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
 chmod 0400 /root/.ssh/authorized_keys
-# Note: After this, use something like
-#   ssh -oStrictHostKeyChecking=no 192.168.17.21 'echo Logging in for host key'
-# to automatically accept the host key for each slave.
-
-echo "hostname: `hostname`"
 
 echo "Installing perl"
 yum -y install perl
@@ -41,8 +37,9 @@ do
   echo "192.168.17.2$i slave-$i" >> /etc/hosts
 done
 
-# Check if this is the master node - that one gets the root private key and the
-# Ambari server package.
+# If this is the master node, copy the root private key to ~/.ssh.
+# Also connect once to all nodes without host checking, in order to
+# automatically accept the host key for each node.
 if [[ `hostname` =~ 'master' ]]
 then
   echo 'Copying private root SSH Key to VM for provisioning...'
@@ -55,6 +52,12 @@ then
   done
 fi
 
+echo "Setting ulimit for open files"
+echo '* hard nofile 10240' >> /etc/security/limits.conf
+echo '* soft nofile 10240' >> /etc/security/limits.conf
+ulimit -Hn 10240
+ulimit -Sn 10240
+
 echo "Creating /gridXX directories"
 for i in 0 1 2
 do
@@ -63,11 +66,25 @@ done
 
 echo "Setting up Ambari repository"
 cd /etc/yum.repos.d
-wget -nc http://public-repo-1.hortonworks.com/ambari/centos6/1.x/updates/1.7.0/ambari.repo
+wget -nv -nc http://public-repo-1.hortonworks.com/ambari/centos6/1.x/updates/1.7.0/ambari.repo
+echo "Installing Ambari agent"
 yum -y install ambari-agent
+
 if [[ `hostname` =~ 'master' ]]
 then
   echo "Installing Ambari server"
   yum -y install ambari-server
+  echo "Setting up Ambari server"
+  ambari-server setup -s
+  echo "Starting Ambari server"
+  ambari-server start
 fi
+
+echo "Waiting for Ambari server to come up"
+while true
+do
+  curl "http://master-1:8080" >&/dev/null && break
+done
+
+echo "Starting Ambari agent"
 
